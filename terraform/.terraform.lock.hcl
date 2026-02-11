3fbe75afdcdc7f4f56f51cd6c859b5b36ef92380
iTzPauG <pgesparterpubli@gmail.com>
2026-02-11 22:37:29 +0100

    fix cloud function

--- terraform/.terraform.lock.hcl
+++ terraform/.terraform.lock.hcl
@@ -5,6 +5,7 @@ provider "registry.terraform.io/hashicorp/archive" {
   version = "2.7.1"
   hashes = [
     "h1:62VrkalDPMKB9zerCBS4iKTbvxejwnAWn/XXYZZQWD4=",
+    "h1:A7EnRBVm4h9ryO9LwxYnKr4fy7ExPMwD5a1DsY7m1Y0=",
     "zh:19881bb356a4a656a865f48aee70c0b8a03c35951b7799b6113883f67f196e8e",
     "zh:2fcfbf6318dd514863268b09bbe19bfc958339c636bcbcc3664b45f2b8bf5cc6",
     "zh:3323ab9a504ce0a115c28e64d0739369fe85151291a2ce480d51ccbb0c381ac5",
@@ -24,6 +25,7 @@ provider "registry.terraform.io/hashicorp/google" {
   version     = "6.50.0"
   constraints = "~> 6.0"
   hashes = [
+    "h1:79CwMTsp3Ud1nOl5hFS5mxQHyT0fGVye7pqpU0PPlHI=",
     "h1:DNt2+WSTnrNlWVUml87IQb+f/aICd3TKETxWIJAELyA=",
     "h1:faTJQOetP9/RYuHwA3r2SWnuYoyzQNm4tUWZrZggcgY=",
     "zh:1f3513fcfcbf7ca53d667a168c5067a4dd91a4d4cccd19743e248ff31065503c",
@@ -45,6 +47,7 @@ provider "registry.terraform.io/hashicorp/google-beta" {
   version = "7.19.0"
   hashes = [
     "h1:Y4U8LRA46ki8NxlpG/l8enJfVXFoE7Yq0Go0JDhTl6Q=",
+    "h1:oJ/+RdbgklEkOTeCzROyxymJGKKCUrQCCT61DfM6gkE=",
     "zh:0db6713b4eef8273e4412c4b55479b38800817eae9155e340532832d52b3d42b",
     "zh:10d8f3ed19ab10fbb258e7e59a2f6f40ddfeac32ebd0fcd21d12e7cd11d1cec8",
     "zh:2017c4b4826d6e713f12184e62041643abf3b5ffff749c230b5b8bf5b136e237",
@@ -57,5 +60,24 @@ provider "registry.terraform.io/hashicorp/google-beta" {
     "zh:be7d64f4c7ad40fa1176ef2fb891fe4c404b307cedca995f4d81cd6229da9dc9",
     "zh:c141da1be759fb27d96910e979114b45984cc17c15e4c6ca0658b71919cd04fd",
     "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.6.2"
+  hashes = [
+    "h1:XaUyyJIDfkkQQ16UopuefD25sfQA6gc653nFGNP6wG0=",
+    "zh:018382c416d271bf89be6fba6550b4fae42e31a4d26a421b3c92f1d589b1b086",
+    "zh:107fd03879eb10d26dcaf5a68d98e4ca7d2488e43c01868e27698d248c1f42a2",
+    "zh:4854946830189100e6a36b8facf028244a7a54baf6e9f5a1a68d11669d8ee597",
+    "zh:4a23aee49842043cb3305f62fa04c3231eb5e6250a439b9a895d8c928a4408a3",
+    "zh:5becec4296cd32ac2b75198ed2de9f716a4c2094ee59d85244d589ae025dfdb6",
+    "zh:619822fb681d7d44c13c1d5ca3d5c85978a8dc8bca18f21bcb80e71816c3f6cd",
+    "zh:6f781cdfde321d530d9b0e4b03ee92a3ec16c82951ab22346ef647d84db321a1",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:8418a84aeed1b31796ffb8c61749406b25e764a4f5a53de29b7ba7c2619692e3",
+    "zh:ab28bd1283712a9720a40c50cc85baffce0f2c23266144602ebbbf1a6a2bccda",
+    "zh:b848cae1d5d1103ba47bd650f27802f316f7bc0dc54df82c514d74269bfcdca5",
+    "zh:bd467d695311c19eec13712792934c19e1b33f351e0cc1fffa08ce404b7d503d",
   ]
 }
--- terraform/cloudsql.tf
+++ terraform/cloudsql.tf
@@ -1,3 +1,90 @@
+# requirements.txt para la Cloud Function Gen 2
+resource "local_file" "zone_data_function_requirements" {
+  filename = "../dataflow-pipeline/requirements.txt"
+  content  = <<-EOT
+psycopg2
+google-cloud-secret-manager
+EOT
+}
+
+# Archivo ZIP para la Cloud Function Gen 2
+resource "archive_file" "zone_data_function_zip" {
+  type        = "zip"
+  output_path = "../dataflow-pipeline/cloud_function_zone_data_to_sql.zip"
+  source {
+    content  = file("../dataflow-pipeline/main.py")
+    filename = "main.py"
+  }
+  source {
+    content  = file("../dataflow-pipeline/requirements.txt")
+    filename = "requirements.txt"
+  }
+  excludes    = ["*.zip"]
+  depends_on  = [local_file.zone_data_function_requirements]
+}
+# Cloud Function Gen 2 para procesar zone data
+resource "google_storage_bucket" "zone_data_function_code" {
+  name     = "${var.cloudsql_instance_name}-zone-data-function-code"
+  location = var.gcp_region
+  force_destroy = true
+}
+
+resource "google_storage_bucket_object" "zone_data_function_zip" {
+  name   = "cloud_function_zone_data_to_sql.zip"
+  bucket = google_storage_bucket.zone_data_function_code.name
+  source = archive_file.zone_data_function_zip.output_path
+}
+
+resource "google_cloudfunctions2_function" "zone_data_to_sql" {
+  name        = "zone-data-to-sql"
+  location    = var.gcp_region
+  description = "Procesa mensajes Pub/Sub y los inserta en Cloud SQL (Gen 2)"
+  build_config {
+    runtime     = "python310"
+    entry_point = "zone_data_to_sql"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.zone_data_function_code.name
+        object = google_storage_bucket_object.zone_data_function_zip.name
+      }
+    }
+  }
+  service_config {
+    min_instance_count = 0
+    max_instance_count = 1
+    available_memory   = "256M"
+    timeout_seconds    = 60
+    environment_variables = {
+      DB_USER    = var.cloudsql_user
+      DB_PASS    = var.cloudsql_password
+      DB_NAME    = var.cloudsql_db_name
+      DB_HOST    = google_sql_database_instance.main.connection_name
+      GCP_PROJECT = var.gcp_project_id
+    }
+  }
+}
+
+# Eventarc trigger para Pub/Sub topic
+resource "google_eventarc_trigger" "zone_data_to_sql_trigger" {
+  name     = "zone-data-to-sql-trigger"
+  location = var.gcp_region
+  transport {
+    pubsub {
+      topic = "projects/${var.gcp_project_id}/topics/zone-data"
+    }
+  }
+  matching_criteria {
+    attribute = "type"
+    value     = "google.cloud.pubsub.topic.v1.messagePublished"
+  }
+    destination {
+      cloud_run_service {
+        service = google_cloudfunctions2_function.zone_data_to_sql.name
+        region  = var.gcp_region
+      }
+    }
+  service_account = google_cloudfunctions2_function.zone_data_to_sql.service_config[0].service_account_email
+}
 # Cloud SQL instance for frequent queries (basic setup)
 resource "google_sql_database_instance" "main" {
   name             = var.cloudsql_instance_name
